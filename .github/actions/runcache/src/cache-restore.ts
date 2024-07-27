c01b5a991dc28fc958154d4f2ca32ba13de53fbe
qawatake <38106890+qawatake@users.noreply.github.com>
2024-07-27 22:30:50 +0900

    runcache

---
+++ .github/actions/runcache/src/cache-restore.ts
@@ -0,0 +1,33 @@
+// some modifications were made to https://github.com/actions/setup-go/tree/v5.0.2/src
+import * as cache from '@actions/cache'
+import * as core from '@actions/core'
+import * as glob from '@actions/glob'
+
+import { State, Outputs } from './constants'
+
+export const restoreCache = async (
+  workflowId: string,
+  jobId: string,
+  cachePath: string
+) => {
+  const platform = process.env.RUNNER_OS
+
+  const linuxVersion =
+    process.env.RUNNER_OS === 'Linux' ? `-${process.env.ImageOS}` : ''
+  const primaryKey = `runcache-${workflowId}-${jobId}-${platform}${linuxVersion}`
+  core.debug(`primary key is ${primaryKey}`)
+
+  core.saveState(State.CachePrimaryKey, primaryKey)
+
+  const cacheKey = await cache.restoreCache([cachePath], primaryKey)
+  core.setOutput(Outputs.CacheHit, Boolean(cacheKey))
+
+  if (!cacheKey) {
+    core.info(`Cache is not found`)
+    core.setOutput(Outputs.CacheHit, false)
+    return
+  }
+
+  core.saveState(State.CacheMatchedKey, cacheKey)
+  core.info(`Cache restored from key: ${cacheKey}`)
+}
